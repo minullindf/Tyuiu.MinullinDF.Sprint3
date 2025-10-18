24f9d5ad7e97237dd065bcddbc839d5010258a92
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-10-18 23:22:29 +0500

    Add project files.

---
+++ Tyuiu.MinullinDF.Sprint3.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.MinullinDF.Sprint3.Task0.V13.Lib
+{
+    public class DataService : ISprint3Task0V13
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double s = 0;
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                s += (Math.Pow(value, k)+0.5)*Math.Cos(k);
+            }
+            return Math.Round(s, 3);
+        }
+    }
+}
---
+++ Tyuiu.MinullinDF.Sprint3.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MinullinDF.Sprint3.Task0.V13.Lib;
+
+namespace Tyuiu.MinullinDF.Sprint3.Task0.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 0.5;
+            int k = 1;
+            var res = ds.GetSumSeries(a, k, 20);
+            double wait = 0.298;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
